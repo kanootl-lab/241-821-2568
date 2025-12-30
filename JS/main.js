01cbb6c48af793e4190d3eae9146a4d9092b4c4e
kanoot <kanoot.l@live.ku.th>
2025-12-30 11:06:14 +0700

    add js

---
+++ JS/main.js
@@ -0,0 +1,97 @@
+/*
+let firstname = "John doe";
+const idcard = '123';
+console.log('Hello, '+ firstName + "!");
+let age = 30;
+let height = 5.9;
+let isStudent = false;
+
+firstname = 'test';
+console.log('My name is', firstname, 'and I am', age, 'years old.');
+*/
+
+/*
++ บวก
+- ลบ
+* คูณ
+/ หาร
+% หารเอาเศษ mod
+*/
+
+/*
+let number1 = 5;
+let number2 = 3;
+
+let result = number1 + number2;
+console.log("ผลบวก =", result);
+*/
+
+
+// Conditional
+/*
+let score = prompt("ป้อนคะแนนของคุณ")
+console.log("คะแนนของคุณ " + score)
+*/
+/*
+let Conditional1 = number1 == number2;
+console.log("result of condition is:", Conditional1);
+*/
+// if - else condition
+
+/*
+let score = 75;
+>=80 เกรดA
+>=70 เกรดb
+>=60 เกรดc
+>=50 เกรดd
+<50  เกรดf
+if (score >= 80) {
+    console.log('เกรดA');
+} else if(score >= 70){
+    console.log('เกรดB');
+}else if(score >=60){
+    console.log('เกรดC');
+}else if(score >= 50){
+    console.log('เกรดD');
+}else{
+    console.log('เกรดF');
+}
+
+/*
+let number1 = 5
+let number2 = 10
+// true && flase
+&& และ
+|| หรือ
+! not ไม่
+let number1 = 5
+let number2 = 10
+    let condition = !(number1 >= 3 || number2 >= 5);
+console.log("condition:", condition);
+
+let age = 30;
+let gender = "ชาย";
+
+// 
+if (age >= 20 && gender == "ชาย"){
+    console.log("ผู้ชายที่มีอายุ 20 ปีขึ้นไป");
+}
+*/
+
+/*
+let number = 20
+
+if (number % 2 !=0 ){
+    console.log("เป็นเลขคู่")
+}
+*/
+
+let counter = -5;
+while(counter < 10){
+    console.log('Hello World');
+    counter = counter + 1;
+}
+
+for(let i=0; i<10; i++){
+    console.log('Hello World from for loop');
+}
