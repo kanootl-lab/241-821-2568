cbe92963e34eb63ac7e2ce1754fdc89f8c9da590
kanoot <kanoot.l@live.ku.th>
2026-01-27 11:42:18 +0700

    add backend

---
+++ Backend/index.js
@@ -0,0 +1,12 @@
+//ทำการimport http module เพื่อสร้าง server
+const http= require('http');
+const host='localhost';
+const port=8000;
+const requestListener= function(req,res){
+    res.writeHead(200);
+    res.end('my first server.');
+}
+const server=http.createServer(requestListener);
+server.listen(port,host,()=>{
+    console.log(`Server is running on http://${host}:${port}`);
+});
