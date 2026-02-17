02b00129ff92cb04568f4bedc81de5bfaf92c49d
kanoot <kanoot.l@live.ku.th>
2026-02-17 11:38:26 +0700

    update apl

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,50 @@
-//ทำการimport http module เพื่อสร้าง server
-const http= require('http');
-const host='localhost';
-const port=8000;
-const requestListener= function(req,res){
-    res.writeHead(200);
-    res.end('my first server.');
-}
-const server=http.createServer(requestListener);
-server.listen(port,host,()=>{
-    console.log(`Server is running on http://${host}:${port}`);
+const express =require('express')
+const bodyParser = require('bodyParser')
+const app = express();
+const port = 8000
+
+app.use(bodyParser.json());
+let users=[];
+let counter =[];
+app.get('/user',(req, res) =>{
+    res.json(users)
+})
+app.post('/user',(req, res) =>{
+let user=req.body;
+user.id = counter
+counter += 1;
+users.push(user)
+res.json({
+    Message:'User',
+    user:user});
+})
+app.put('/user',(req, res) =>{
+    let id = req.params.id;
+    let updateUser = req.body
+    let seletedindex = users.findIndex(user => user.id == id)
+    if(updateUser.email){
+          users[seletedindex].email=updateUser.email;
+    }
+    if(updateUser.name){
+          users[seletedindex].name=updateUser.name;
+    }
+    res.json({
+        Message:' user successfully',
+        data :{
+            users: updateUser,
+            indexupdated : seletedindex
+        }
+    })
+    })
+    app.delete('/user',(req, res) =>{
+        let id = req.params.id;
+        let seletedindex = users.findIndex(user => user.id == id)
+        delete users[seletedindex]
+        res.json({
+            Message:'user delete successfully',
+            indexdelete:seletedindex
+        })
+    })
+app.listen(port, () =>{
+    console.log('server is runing on port${port}')
 });
